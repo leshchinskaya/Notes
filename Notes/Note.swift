2d6c90a89dd50eb9135b989165c9a7bbbefbce9c
Leshchinskaya <mariya@leshchinskaya.local>
2017-10-08 02:22:28 +0300

    simple Notes work

---
+++ Notes/Note.swift
@@ -0,0 +1,14 @@
+//
+//  Note.swift
+//  Notes
+//
+//  Created by Marie on 07.10.17.
+//  Copyright © 2017 Mariya. All rights reserved.
+//
+
+import Foundation
+
+class Note {
+    var title = ""
+    var content = ""
+}
---
+++ Notes/NoteDetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  NoteDetailViewController.swift
+//  Notes
+//
+//  Created by Marie on 07.10.17.
+//  Copyright © 2017 Mariya. All rights reserved.
+//
+
+import UIKit
+
+class NoteDetailViewController: UIViewController {
+    
+    
+    @IBOutlet weak var contentTextField: UITextField!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    var note: Note!
+    
+    @IBOutlet weak var contentTextView: UITextView!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        titleTextField.text = note.title
+        //contentTextField.text = note.content
+        contentTextView.text = note.content
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        note.title = titleTextField.text!
+        //note.content = contentTextField.text!
+        note.content = contentTextView.text!
+    }
+    
+}
